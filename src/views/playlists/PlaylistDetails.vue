90a58f732edb6387fee6dede5709dacaa9737cd8
alexquar <alexanderquarrie@gmail.com>
2024-03-18 15:01:59 -0400

    deleting images from storage

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -22,24 +22,30 @@
   </template>
   
   <script>
+  import useStorage from '@/composables/useStorage'
   import getDocument from '@/composables/getDocument'
   import getUser from '@/composables/getUser'
   import useDocument from '@/composables/useDocument'
   import {computed} from 'vue'
+  import {useRouter} from 'vue-router'
   
   export default {
     props: ['id'],
     setup(props) {
       const { error, document: playlist } = getDocument('playlists', props.id)
       const {user} = getUser()
+      const {deleteImage} = useStorage()
       const {deleteDoc} = useDocument('playlists', props.id)
+      const router = useRouter()
       const ownership = computed(()=>{
 
         return playlist.value && user.value && user.value.uid == playlist.value.userId
       })
 
       const handleDelete = async () => {
+        await deleteImage(playlist.value.filePath)
         await deleteDoc()
+        router.push({name : 'Home'})
       }
 
       return { error, playlist, ownership, handleDelete }
