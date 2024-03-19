efa7c1de51bf667513c80b543965d4db442be9b2
alexquar <alexanderquarrie@gmail.com>
2024-03-19 09:01:32 -0400

    deleting songs

--- src/views/playlists/PlaylistDetails.vue
+++ src/views/playlists/PlaylistDetails.vue
@@ -5,109 +5,122 @@
       <!-- playlist information -->
       <div class="playlist-info">
         <div class="cover">
-          <img :src="playlist.coverUrl" alt="playlist cover image">
+          <img :src="playlist.coverUrl">
         </div>
         <h2>{{ playlist.title }}</h2>
         <p class="username">Created by {{ playlist.userName }}</p>
         <p class="description">{{ playlist.description }}</p>
-        <button @click = "handleDelete" v-if="ownership"> Delete Playlist</button>
+        <button v-if="ownership" @click="handleDelete">Delete Playlist</button>
       </div>
   
       <!-- song list -->
       <div class="song-list">
-      <div v-if="!playlist.songs.length">No songs have been added to this playlist yet.</div>
-      <div v-for="song in playlist.songs" :key="song.id" class="single-song">
-        <div class="details">
-          <h3>{{ song.title }}</h3>
-          <p>{{ song.artist }}</p>
+        <div v-if="!playlist.songs.length">No songs have been added to this playlist yet.</div>
+        <div v-for="song in playlist.songs" :key="song.id" class="single-song">
+          <div class="details">
+            <h3>{{ song.title }}</h3>
+            <p>{{ song.artist }}</p>
+          </div>
+          <button v-if="ownership" @click="handleClick(song.id)">delete</button>
         </div>
-        <button v-if="ownership">delete</button>
+        <AddSong :playlist="playlist" />
       </div>
-      <AddSong :playlist="playlist" />
-    </div>
       
     </div>
   </template>
   
   <script>
+  // challenge
+  //   - attach a click handler to the delete (song) button called handleClick
+  //   - inside the function, use the updateDoc function to delete that song
+  //   - you'll need to pass the song id into the handleClick function
+  //   - HINT: use the filter method
+  
   import AddSong from '@/components/AddSong.vue'
   import useStorage from '@/composables/useStorage'
+  import useDocument from '@/composables/useDocument'
   import getDocument from '@/composables/getDocument'
   import getUser from '@/composables/getUser'
-  import useDocument from '@/composables/useDocument'
-  import {computed} from 'vue'
-  import {useRouter} from 'vue-router'
+  import { computed } from 'vue'
+  import { useRouter } from 'vue-router'
   
   export default {
     props: ['id'],
-    components : {AddSong},
+    components: { AddSong },
     setup(props) {
       const { error, document: playlist } = getDocument('playlists', props.id)
-      const {user} = getUser()
-      const {deleteImage} = useStorage()
-      const {deleteDoc} = useDocument('playlists', props.id)
+      const { user } = getUser()
+      const { deleteDoc, updateDoc } = useDocument('playlists', props.id)
+      const { deleteImage } = useStorage()
       const router = useRouter()
-      const ownership = computed(()=>{
-
-        return playlist.value && user.value && user.value.uid == playlist.value.userId
+  
+      const ownership = computed(() => {
+        return playlist.value 
+          && user.value 
+          && user.value.uid == playlist.value.userId
       })
-
+  
       const handleDelete = async () => {
-        await deleteImage(playlist.value.filePath)
         await deleteDoc()
-        router.push({name : 'Home'})
+        await deleteImage(playlist.value.filePath)
+        router.push({ name: 'Home' })
       }
-
-      return { error, playlist, ownership, handleDelete }
+  
+      const handleClick = async (id) => {
+        const songs = playlist.value.songs.filter((song) => song.id != id)
+        await updateDoc({ songs })
+      }
+  
+      return { error, playlist, ownership, handleDelete, handleClick }
     }
   }
   </script>
   
   <style>
-  .playlist-details {
-    display: grid;
-    grid-template-columns: 1fr 2fr;
-    gap: 80px;
-  }
-  .cover {
-    overflow: hidden;
-    border-radius: 20px;
-    position: relative;
-    padding: 160px;
-  }
-  .cover img {
-    display: block;
-    position: absolute;
-    top: 0;
-    left: 0;
-    min-width: 100%;
-    min-height: 100%;
-    max-width: 200%;
-    max-height: 200%;
-  }
-  .playlist-info {
-    text-align: center;
-  }
-  .playlist-info h2 {
-    text-transform: capitalize;
-    font-size: 28px;
-    margin-top: 20px;
-  }
-  .playlist-info p {
-    margin-bottom: 20px;
-  }
-  .username {
-    color: #999;
-  }
-  .description {
-    text-align: left;
-  }
-  .single-song {
-    padding: 10px 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    border-bottom: 1px dashed var(--secondary);
-    margin-bottom: 20px;
-  }
+    .playlist-details {
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+      gap: 80px;
+    }
+    .cover {
+      overflow: hidden;
+      border-radius: 20px;
+      position: relative;
+      padding: 160px;
+    }
+    .cover img {
+      display: block;
+      position: absolute;
+      top: 0;
+      left: 0;
+      min-width: 100%;
+      min-height: 100%;
+      max-width: 200%;
+      max-height: 200%;
+    }
+    .playlist-info {
+      text-align: center;
+    }
+    .playlist-info h2 {
+      text-transform: capitalize;
+      font-size: 28px;
+      margin-top: 20px;
+    }
+    .playlist-info p {
+      margin-bottom: 20px;
+    }
+    .username {
+      color: #999;
+    }
+    .description {
+      text-align: left;
+    }
+    .single-song {
+      padding: 10px 0;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-bottom: 1px dashed var(--secondary);
+      margin-bottom: 20px;
+    }
   </style>
